c93d2a2a3aebe738931bf77f40e6bb4fb5decd90
andrew.s.dterra <a.shirin@dterra.eu>
2022-07-12 22:21:30 +0300

    third commit

--- config/server.js
+++ config/server.js
@@ -2,9 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: 'http://86679.web.hosting-russia.ru/',
-  admin: {
-    url: 'http://86679.web.hosting-russia.ru/admin/',
-  },
+  proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
   },
